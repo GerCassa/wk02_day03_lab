3f8a3c468853ad22c48cf102ce1314d87659dd2b
GerCassa <gercassa@gmail.com>
2018-08-08 14:13:47 +0100

    passed test_customer_has_name

--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -7,11 +7,15 @@ require_relative('../customer')
 class CustomerTest < MiniTest::Test
 
   def setup
-    @customer = Customer.new("Tony")
+    @customer = Customer.new("Tony", 50)
   end
 
   def test_customer_has_name
     assert_equal("Tony", @customer.name)
+  end
+
+  def test_has_wallet_with_money
+    assert_equal(50, @customer.wallet)
   end
 
 end
