85d2e4706d18bae8cfa3e64d4b9c7ccf83f57d47
GerCassa <gercassa@gmail.com>
2018-08-08 13:54:45 +0100

    passed test_get_drink_by_name

--- pub.rb
+++ pub.rb
@@ -7,4 +7,15 @@ class Pub
     @till = 0
     @drinks = drinks
   end
+
+  def get_drink(name)
+    for drink in @drinks
+      if drink.name == name
+        return drink
+      end
+    end
+    return nil
+  end
+
+
 end
